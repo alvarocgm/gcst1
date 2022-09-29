b185e6ed71d417e95abe88f73eb1cc3656db3dea
Álvaro Machado <alvarocgm@gmail.com>
2022-09-29 02:29:13 -0300

    Update App.java

--- src/main/App.java
+++ src/main/App.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class App {
+public class main {
     public static void main(String[] args) throws IOException {
         Scanner leitor = new Scanner(System.in);
     	int X, Y, soma;
